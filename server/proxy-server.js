37d8eb94f6d3e9ed86b0d03f2c38b8c5c8b9aa8c
John Yi <john.yi1411@gmail.com>
2019-09-17 20:57:57 -0700

    Add morgan to server

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const bodyParser = require('body-parser');
 
 const port = 3000;
 
-
-app.use('/:id/', express.static('public'));
+app.use(morgan('tiny'));
+app.use('/restaraunt/:id/', express.static('public'));
 
 
 app.listen(port, () => { console.log(`Proxy server listening on port ${port}`); });
